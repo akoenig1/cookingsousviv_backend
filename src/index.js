914cba92917870e7bf1f41e261cd02b33c58ffa2
Alex Koenig <alex.koenig4@gmail.com>
2020-10-22 14:02:48 -0400

    Modify CORS settings

--- src/index.js
+++ src/index.js
@@ -22,7 +22,14 @@ var corsOptions = {
 
 
 // Imported middleware //
-app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
