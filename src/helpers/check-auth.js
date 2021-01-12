365f613ccb1aa4c514c18c60914c6e8bf59d52fe
Alex Koenig <alex.koenig4@gmail.com>
2021-01-11 23:45:38 -0500

    Removed unnecessary logging from check-auth

--- src/helpers/check-auth.js
+++ src/helpers/check-auth.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 
 module.exports = (req, res, next) => {
     try {
-        console.log(req.headers.authorization)
         const token = req.headers.authorization.split(' ')[1];
         if(!token) {
             throw new Error('Authentication failed!');
